a33e6ca2ce93f081755128a9bc7803ed716c4896
NKDuy <kn145660@gmail.com>
2022-12-15 17:01:16 +0700

    fix git commit msg

--- scripts/release.js
+++ scripts/release.js
@@ -80,7 +80,7 @@ const release = async () => {
     })
     delete process.env.PREFIX
     await execa('git', ['add', '-A'], { stdio: 'inherit' })
-    await execa('git', ['commit', '-m', '"chore: pre release sync"'], { stdio: 'inherit' })
+    await execa('git', ['commit', '-m', 'chore: pre release sync'], { stdio: 'inherit' })
   }
 
   await execa(require.resolve('lerna/bin/lerna'), [
