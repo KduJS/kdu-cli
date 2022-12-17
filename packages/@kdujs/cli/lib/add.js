c4a28ce72483c9d0750eec1548644df481f3e665
NKDuy <kn145660@gmail.com>
2022-12-17 09:48:18 +0700

    fix: fix `kdu add router` command in v3 projects

--- packages/@kdujs/cli/lib/add.js
+++ packages/@kdujs/cli/lib/add.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk')
 const semver = require('semver')
 const invoke = require('./invoke')
+const inquirer = require('inquirer')
+const { loadModule } = require('@kdujs/cli-shared-utils')
 
 const PackageManager = require('./util/ProjectPackageManager')
 const {
@@ -14,6 +16,18 @@ const confirmIfGitDirty = require('./util/confirmIfGitDirty')
 async function add (pluginName, options = {}, context = process.cwd()) {
   if (!(await confirmIfGitDirty(context))) {
     return
+  }
+
+  // for `kdu add` command in 3.x projects
+  const servicePkg = loadModule('@kdujs/cli-service/package.json', context)
+  if (semver.satisfies(servicePkg.version, '3.x')) {
+    // special internal "plugins"
+    if (/^(@kdujs\/)?router$/.test(pluginName)) {
+      return addRouter(context)
+    }
+    if (/^(@kdujs\/)?kdux$/.test(pluginName)) {
+      return addKdux(context)
+    }
   }
 
   const packageName = resolvePluginId(pluginName)
@@ -43,5 +57,25 @@ module.exports = (...args) => {
     if (!process.env.KDU_CLI_TEST) {
       process.exit(1)
     }
+  })
+}
+
+async function addRouter (context) {
+  const options = await inquirer.prompt([{
+    name: 'routerHistoryMode',
+    type: 'confirm',
+    message: `Use history mode for router? ${chalk.yellow(`(Requires proper server setup for index fallback in production)`)}`
+  }])
+  invoke.runGenerator(context, {
+    id: 'core:router',
+    apply: loadModule('@kdujs/cli-service/generator/router', context),
+    options
+  })
+}
+
+async function addKdux (context) {
+  invoke.runGenerator(context, {
+    id: 'core:kdux',
+    apply: loadModule('@kdujs/cli-service/generator/kdux', context)
   })
 }
